507bad1d659cf95b058c93bfd116b1f206e90dc3
Reid <rlj0713@gmail.com>
2022-01-11 18:50:02 -0500

    iterator that fails

--- prime.py
+++ prime.py
@@ -2,7 +2,7 @@ import time
 
 start_time = time.perf_counter()
 
-list_of_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73]
+list_of_primes = [2, 3, 5, 7]
 
 def is_prime(x):
   
@@ -17,13 +17,14 @@ def is_prime(x):
   if count <= 1:
     return True
 
-j = 2
-while len(list_of_primes) <= 10001:
-  if j % 2 != 0 and j % 3 != 0 and j % 5 != 0 and j % 7 != 0 and j % 11 != 0 and j % 13 != 0 and j % 17 != 0 and j % 19 != 0 and j % 23 != 0 and j % 29 != 0 and j % 31 != 0 and j % 37 != 0 and j % 41 != 0 and j % 43 != 0 and j % 47 != 0 and j % 53 != 0 and j % 59 != 0 and j % 61 != 0 and j % 67 != 0 and j % 71 != 0 and j % 73 != 0 and is_prime(j):  
-    list_of_primes.append(j)
-  j += 1
+j = 8
+while len(list_of_primes) <= 6:
+  for k in list_of_primes:
+    if j % k != 0 and is_prime(j):  
+      list_of_primes.append(j)
+    j += 1
 
-print(list_of_primes[-2])
+print(list_of_primes)
 
 
 end_time = time.perf_counter()
