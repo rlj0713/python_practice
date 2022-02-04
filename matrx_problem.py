fd4d89e0f9f9c283290daf9e29f710e264f55dce
Reid <rlj0713@gmail.com>
2022-02-04 11:00:57 -0500

    duplicated example problem

--- matrx_problem.py
+++ matrx_problem.py
@@ -47,10 +47,31 @@ generate_route_list()
 # Use a unique? comparison to populate the list without repeating.
 list_of_all_possible_routes = []
 
-while len(list_of_all_possible_routes) < 18:
+while len(list_of_all_possible_routes) < 70:
     this_route = generate_route_list()
     
-    if this_route not in list_of_all_possible_routes:
+    if this_route not in list_of_all_possible_routes and this_route[-1] == 55:
         list_of_all_possible_routes.append(this_route)
 
-print(list_of_all_possible_routes)
+# print(list_of_all_possible_routes)
+
+def master_route_list_with_v(list_of_all_possible_routes):
+    final_array = []
+    for a in list_of_all_possible_routes:
+        altered_array = []
+        for i in a:
+            altered_array.append('v' + str(i))
+        final_array.append(altered_array)
+    return(final_array)
+
+all_routes = master_route_list_with_v(list_of_all_possible_routes)
+
+array_of_sums = []
+for r in all_routes:
+    this_array_total = 0
+    for i in r:
+        this_array_total += eval(i).value
+    array_of_sums.append(this_array_total)
+
+# This successfully duplicates the example
+print(sorted(array_of_sums)[0])
