8c911bca8fd3e09dafab7ac7ff127f87f6588b59
Reid <rlj0713@gmail.com>
2022-02-24 09:48:25 -0500

    established starting point and end point

--- lattice_paths.py
+++ lattice_paths.py
@@ -1,1 +1,5 @@
 # Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
+
+starting_point = [0, 0]
+ending_point = [2, 2]
+
