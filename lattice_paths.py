dcacf1945dd34b9d2bc16e2203fb1e277c5efb36
Reid <rlj0713@gmail.com>
2022-02-24 09:50:33 -0500

    established random index

--- lattice_paths.py
+++ lattice_paths.py
@@ -1,5 +1,8 @@
 # Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 
+import random
+
 starting_point = [0, 0]
 ending_point = [2, 2]
 
+print(random.randint(0, 1))
